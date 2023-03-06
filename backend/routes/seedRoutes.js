263a50d7f79b8e0197b2d2e9244caa3a4c80e34d
laurisseau <reso0208@gmail.com>
2023-03-06 00:16:11 -0500

    asdf

--- backend/routes/seedRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { seed } from '../controller/seedController.js';
-
-const seedRouter = express.Router();
-
-seedRouter.get('/', seed);
-
-export default seedRouter;
